a8bd8b1a64bc8fe8052634d228f4b886c33894a6
Phelipe Folgierini <phelipe.folgierini@basis.com.br>
2017-04-08 17:05:59 -0300

    Ajustes scrap user info

--- handlers/fb_handlers.php
+++ handlers/fb_handlers.php
@@ -131,7 +131,7 @@ function get_user_info($accessToken)
         $arrayGraphEdge = extract_fb_data('me', ['id','name','birthday','about','age_range','gender','locale',/*'context'*/], 25, $accessToken);
 
         #print_r($arrayGraphEdge); die;
-        #print_r($arrayGraphEdge['context']); die;
+        print_r($arrayGraphEdge['birthday']); die;
 
         $params = null;
         $params = [];
